ff220a3593e35a39b0eeea2bfed1317c460ccc4c
Romain M. <88449922+4strium@users.noreply.github.com>
2024-08-04 09:46:29 +0200

    Add error handling for "enregistrement.wav" in Python script

--- Wav-server/server-side/recognize-word.py
+++ Wav-server/server-side/recognize-word.py
@@ -4,21 +4,27 @@ import os
 current_dir = os.getcwd()
 filename = current_dir + "/enregistrement.wav"
 
-r = sr.Recognizer()
-with sr.AudioFile(filename) as source:
-    audio = r.record(source)
-    try:
-        datafr = r.recognize_google(audio, language="fr-FR")
-        print("Reconnaissance réussie : ", datafr)
-    except:
-        print("Ressayez s'il vous plaît...")
-        datafr = "Erreur de reconnaissance"
+# Check if the file is empty
+if os.path.getsize(filename) == 0:
+    print("Le fichier est vide, aucun traitement effectué.")
+    with open("rapport.txt", "w") as file:
+        file.write("Erreur de reconnaissance\n")
+else:
+    r = sr.Recognizer()
+    with sr.AudioFile(filename) as source:
+        audio = r.record(source)
+        try:
+            datafr = r.recognize_google(audio, language="fr-FR")
+            print("Reconnaissance réussie : ", datafr)
+        except sr.UnknownValueError:
+            print("Ressayez s'il vous plaît...")
+            datafr = "Erreur de reconnaissance"
 
-with open("rapport.txt", "w") as file:
-    file.write(datafr)
-    file.write("\n")
+    with open("rapport.txt", "w") as file:
+        file.write(datafr)
+        file.write("\n")
 
-# Vérifiez le contenu du fichier après l'écriture
-with open("rapport.txt", "r") as file:
-    content = file.read()
-    print("Contenu du fichier :", repr(content))
+    # Vérifiez le contenu du fichier après l'écriture
+    with open("rapport.txt", "r") as file:
+        content = file.read()
+        print("Contenu du fichier :", repr(content))
